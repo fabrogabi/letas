a4664f04f2f4e07e163a0d19f0d3da2ec4cdff13
fabrogabi <fabrogabi@gmail.com>
2021-02-24 19:43:17 +0100

    a

--- src/Components/Formulario.js
+++ src/Components/Formulario.js
@@ -1,51 +1,90 @@
-import React from 'react';
+import React,{useState} from 'react';
+import Error from './Error'
 
 
 const Formulario = () => {
+
+    const [busqueda,guardarBusqueda]=useState({cancion:'',artista:''})
+    const [error , guardarError]=useState(false)
+
+    const handleInputChange=e=>{
+        guardarBusqueda({...busqueda,
+            [e.target.name]:e.target.value
+
+        })
+    }
+
+    const handleSubmit = e=>{
+        e.preventDefault();
+        if(artista===''||cancion==='')
+        {
+            guardarError(true);
+            return;
+        }
+             
+    }
+
+    const {artista ,cancion}=busqueda
+
+
+
     
     return ( 
 
+ 
         <div className="bg-info">
+           
             <div className="container">
                 <div className="row">
-                    <form
-                        className="col card text-white gb-transparent mb-5 pt-5 pb-2"
+                    {error? <Error mensaje="complete los todos los campos vacios"/> :null}
+                    <form 
+                         className="col card text-white bg-transparent mb-5 pt-5 pb-2"
+                         onSubmit={handleSubmit}
                     >
                         <fieldset>
-                            <legend className="text-center">Buscador de Canciones</legend>
+                            <legend className="text-center">Buscador Letras Canciones</legend>
+
                             <div className="row">
                                 <div className="col-md-6">
                                     <div className="form-group">
                                         <label>Artista</label>
-                                        <input 
+                                        <input
                                             type="text"
                                             className="form-control"
                                             name="artista"
-                                            placeholder="Nombre del Artista"
-                                        />
+                                            placeholder="Nombre Artista"
+                                            onChange={handleInputChange}
+                                            value={artista}                                            
+                                            />
                                     </div>
+                                    
                                 </div>
                                 <div className="col-md-6">
                                     <div className="form-group">
-                                            <label>Cancion</label>
-                                            <input 
-                                                type="text"
-                                                className="form-control"
-                                                name="cancion"
-                                                placeholder="Nombre cancion"
-                                            />
+                                        <label>Canción</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="cancion"
+                                            placeholder="Nombre Canción"
+                                            onChange={handleInputChange}
+                                            value={cancion}
+                                        />
                                     </div>
                                 </div>
                             </div>
+
+                            <button 
+                                type="submit" 
+                                className="btn btn-primary float-right"
+                            >Buscar</button>
                         </fieldset>
-
-                    
-
 
                     </form>
                 </div>
             </div>
         </div>
+   
      );
 }
  
